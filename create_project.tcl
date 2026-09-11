3779efaf16b478cf412d28da4fd21d1f9b47484f
LynVii <84234492+LynViii@users.noreply.github.com>
2026-09-11 15:09:37 +0800

    Recreate Block Memory IP from Tcl instead of committing XCI files

--- create_project.tcl
+++ create_project.tcl
@@ -6,12 +6,34 @@ set_property target_language Verilog [current_project]
 set_property simulator_language Mixed [current_project]
 
 add_files [glob -nocomplain [file join $script_dir rtl *.v]]
-add_files [file join $script_dir ip blk_ram blk_ram.xci]
-add_files [file join $script_dir ip blk_rom blk_rom.xci]
 
-# Keep the COE paths local to the repository rather than the original desktop path.
-set_property CONFIG.Coe_File [file normalize [file join $script_dir ip blk_ram ram.coe]] [get_ips blk_ram]
-set_property CONFIG.Coe_File [file normalize [file join $script_dir ip blk_rom rom.coe]] [get_ips blk_rom]
+# Re-create the two Block Memory Generator IPs from the original project settings.
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -module_name blk_ram
+set_property -dict [list \
+    CONFIG.Memory_Type {Single_Port_RAM} \
+    CONFIG.Write_Width_A {16} \
+    CONFIG.Write_Depth_A {256} \
+    CONFIG.Read_Width_A {16} \
+    CONFIG.Operating_Mode_A {WRITE_FIRST} \
+    CONFIG.Enable_A {Always_Enabled} \
+    CONFIG.Use_Byte_Write_Enable {false} \
+    CONFIG.Register_PortA_Output_of_Memory_Primitives {true} \
+    CONFIG.Register_PortA_Output_of_Memory_Core {false} \
+    CONFIG.Load_Init_File {true} \
+    CONFIG.Coe_File [file normalize [file join $script_dir ip blk_ram ram.coe]] \
+] [get_ips blk_ram]
+
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -module_name blk_rom
+set_property -dict [list \
+    CONFIG.Memory_Type {Single_Port_ROM} \
+    CONFIG.Write_Width_A {32} \
+    CONFIG.Write_Depth_A {256} \
+    CONFIG.Enable_A {Always_Enabled} \
+    CONFIG.Register_PortA_Output_of_Memory_Primitives {true} \
+    CONFIG.Register_PortA_Output_of_Memory_Core {false} \
+    CONFIG.Load_Init_File {true} \
+    CONFIG.Coe_File [file normalize [file join $script_dir ip blk_rom rom.coe]] \
+] [get_ips blk_rom]
 
 generate_target all [get_ips blk_ram]
 generate_target all [get_ips blk_rom]
